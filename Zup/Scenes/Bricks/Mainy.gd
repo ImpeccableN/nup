89b0ccb1c8db9fdb6617ab15f6998820bfb07ef0
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-12-23 20:01:32 +0100

    added states to Mainy and with Statechange the Sprite changes

--- Zup/Scenes/Bricks/Mainy.gd
+++ Zup/Scenes/Bricks/Mainy.gd
@@ -4,6 +4,14 @@ var impulse_force = 4000
 var last_position
 var checking_floor = false
 
+var current_state
+
+var img_default = preload("res://Assets/Img/nick_default.png")
+var img_laughing = preload("res://Assets/Img/nick_laughing.png")
+var img_shades = preload("res://Assets/Img/nick_shades.png")
+
+enum STATE {DEFAULT, FLYING, WINNING}
+
 
 func add_explosion(block):
 	apply_central_impulse((transform.origin - block.transform.origin).normalized() * impulse_force)
@@ -11,18 +19,27 @@ func add_explosion(block):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	current_state = STATE.DEFAULT
 
 
-func _process(delta):
-	pass
-
+func _physics_process(delta):
+	if current_state != STATE.FLYING:
+		if $FloorChecker.get_overlapping_bodies().empty():
+			print("empty")
+			$Sprite.set_texture(img_laughing)
+			current_state = STATE.FLYING
+	elif current_state == STATE.FLYING:
+		if not $FloorChecker.get_overlapping_bodies().empty():
+			print("not empty")
+			$Sprite.set_texture(img_default)
+			current_state = STATE.DEFAULT
 
 func check_for_target():
 	checking_floor = true
 	var entered_bodies_array = $FloorChecker.get_overlapping_bodies()
 	for body in entered_bodies_array:
 		if body.is_in_group("target"):
+			$Sprite.set_texture(img_shades)
 			$Timer.stop()
 			print("you win")
 #			implement win_func()
